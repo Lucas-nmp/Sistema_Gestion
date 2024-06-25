fe5dec949a848642d72f16790790db090fe82eaa
Lucas-nmp <lucas.morandeira.parejo@gmail.com>
2024-06-25 22:44:23 +0200

    Creación del paquete servicio para usar los métodos importados de JPA mediante una interfaz que define los métodos que vamos a necesitar en la clase servicio, User de momento

---
+++ src/main/java/com/sistema/Gestion/service/IUserService.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.sistema.Gestion.service;
+
+import com.sistema.Gestion.model.User;
+import java.util.List;
+
+/**
+ *
+ * @author Lucas
+ */
+public interface IUserService {
+    
+    public List<User> viewUsers();
+    
+    public User findUserById(Integer idUser);
+    
+    public void addModifyUser(User user);
+    
+    public void deleteUser(User user);
+    
+}
---
+++ src/main/java/com/sistema/Gestion/service/UserService.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sistema.Gestion.service;
+
+import com.sistema.Gestion.model.User;
+import com.sistema.Gestion.repository.UserRepository;
+import java.util.List;
+
+/**
+ *
+ * @author Lucas
+ */
+public class UserService implements IUserService{
+
+    private UserRepository userRepository;
+    
+    @Override
+    public List<User> viewUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User findUserById(Integer idUser) {
+        User user = userRepository.findById(idUser).orElse(null);
+        return user;
+    }
+
+    @Override
+    public void addModifyUser(User user) {
+        // de forma automática detecta si el id del usuario existe o no para modificarlo o crearlo nuevo
+        userRepository.save(user);
+        
+    }
+
+    @Override
+    public void deleteUser(User user) {
+        userRepository.delete(user);
+        
+        
+    }
+    
+}
